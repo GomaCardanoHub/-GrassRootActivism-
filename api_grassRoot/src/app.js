725a1738600d4e9b58eb5fe81d79d056105f104f
Akilimaili cizungu <gentilakili98@gmail.com>
2022-06-13 22:48:44 +0200

    init model database

--- api_grassRoot/src/app.js
+++ api_grassRoot/src/app.js
@@ -1,8 +1,6 @@
 const express = require('express');
-
-
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.listen(port,()=>console.log(`Vous etec sur le serveur ${port}`));
+app.listen(port,()=>console.log(`Vous êtes sur le serveur ${port}`));
