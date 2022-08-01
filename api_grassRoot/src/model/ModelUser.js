fef1649169a2ed55c4575a09a01fa956096ac31f
Akim <gentilakili98@gmail.com>
2022-08-01 20:46:14 +0200

    send all correction in add all router && add  jwt

---
+++ api_grassRoot/src/model/ModelUser.js
@@ -0,0 +1,14 @@
+module.exports = (sequelize, DataTypes) => {
+    const user = sequelize.define("Users", {
+      code: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: true,
+        autoIncrement: true,
+      },
+      username: DataTypes.STRING,
+      password: DataTypes.STRING,
+    });
+    return user;
+  };
+  
